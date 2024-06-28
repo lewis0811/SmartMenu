b05f1a3bfad5bceeb915e5527f3c2bf6648724c8
pytas0811 <tekato2002@gmail.com>
2024-06-29 01:42:08 +0700

    Update BoxService

---
+++ SmartMenu.Service/Interfaces/IBoxService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SmartMenu.Domain.Models;
+using SmartMenu.Domain.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenu.Service.Interfaces
+{
+    public interface IBoxService
+    {
+        IEnumerable<Box> GetAll(int? boxId, int? layerId, string? searchString, int pageNumber, int pageSize);
+        IEnumerable<Box> GetAllWithBoxItems(int? boxId, int? layerId, string? searchString, int pageNumber, int pageSize);
+        Box Add(BoxCreateDTO boxCreateDTO);
+        Box Update(int boxId, BoxUpdateDTO boxUpdateDTO);
+        void Delete(int boxId);
+    }
+}
