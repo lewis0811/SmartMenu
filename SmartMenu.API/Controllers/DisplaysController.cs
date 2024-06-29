47e57f8c3ddc096fb6503f3518da9815bd1a579e
pytas0811 <tekato2002@gmail.com>
2024-06-29 12:06:18 +0700

    Add DisplayService

--- SmartMenu.API/Controllers/DisplaysController.cs
+++ SmartMenu.API/Controllers/DisplaysController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using SmartMenu.Domain.Models;
 using SmartMenu.Domain.Models.DTO;
 using SmartMenu.Domain.Repository;
+using SmartMenu.Service.Interfaces;
 
 namespace SmartMenu.API.Controllers
 {
@@ -11,52 +12,56 @@ namespace SmartMenu.API.Controllers
     [ApiController]
     public class DisplaysController : ControllerBase
     {
-        private readonly IMapper _mapper;
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDisplayService _displayService;
 
-        public DisplaysController(IMapper mapper, IUnitOfWork unitOfWork)
+        public DisplaysController(IDisplayService displayService)
         {
-            _mapper = mapper;
-            _unitOfWork = unitOfWork;
+            _displayService = displayService;
         }
 
         [HttpGet]
         public IActionResult Get(int? displayId, int? menuId, int? collectionId, string? searchString, int pageNumber = 1, int pageSize = 10)
         {
-            var data = _unitOfWork.DisplayRepository.GetAll(displayId, menuId, collectionId, searchString, pageNumber, pageSize);
-            data ??= Enumerable.Empty<Display>();
+            try
+            {
+                var data = _displayService.GetAll(displayId, menuId, collectionId, searchString, pageNumber, pageSize);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
 
-            return Ok(data);
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("DisplayItems")]
         public IActionResult GetWithItems(int? displayId, int? menuId, int? collectionId, string? searchString, int pageNumber = 1, int pageSize = 10)
         {
-            var data = _unitOfWork.DisplayRepository.GetWithItems(displayId, menuId, collectionId, searchString, pageNumber, pageSize);
-            data ??= Enumerable.Empty<Display>();
+            try
+            {
+                var data = _displayService.GetWithItems(displayId, menuId, collectionId, searchString, pageNumber, pageSize);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
 
-            return Ok(data);
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
         public IActionResult Add(DisplayCreateDTO displayCreateDTO)
         {
-            if (displayCreateDTO.MenuId != null && displayCreateDTO.CollectionId != null) return BadRequest("MenuId and CollectionId cannot be using at the same time");
+            try
+            {
+                var data = _displayService.AddDisplay(displayCreateDTO);
+                return CreatedAtAction(nameof(Get), data);
+            }
+            catch (Exception ex)
+            {
 
-            var storeDevice = _unitOfWork.StoreDeviceRepository.Find(c => c.StoreDeviceId == displayCreateDTO.StoreDeviceId && c.IsDeleted == false).FirstOrDefault();
-            var menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayCreateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
-            var collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayCreateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
-            var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayCreateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
-
-            if (storeDevice == null) return BadRequest("StoreDevice not found or deleted");
-            if (menu == null) return BadRequest("Menu not found or deleted");
-            if (collection == null) return BadRequest("Collection not found or deleted");
-            if (template == null) return BadRequest("Template not found or deleted");
-
-            var data = _mapper.Map<Display>(displayCreateDTO);
-            _unitOfWork.DisplayRepository.Add(data);
-            _unitOfWork.Save();
-            return CreatedAtAction(nameof(Get), data);
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("V2")]
@@ -65,100 +70,7 @@ namespace SmartMenu.API.Controllers
         {
             try
             {
-                if (displayCreateDTO.MenuId == 0) displayCreateDTO.MenuId = null;
-                if (displayCreateDTO.CollectionId == 0) displayCreateDTO.CollectionId = null;
-
-                var storeDevice = _unitOfWork.StoreDeviceRepository.Find(c => c.StoreDeviceId == displayCreateDTO.StoreDeviceId && c.IsDeleted == false).FirstOrDefault();
-                var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayCreateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
-
-                var menu = new Menu();
-                if (displayCreateDTO.MenuId != null)
-                {
-                    menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayCreateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
-                }
-                else { menu = null; }
-
-                var collection = new Collection();
-                if (displayCreateDTO.CollectionId != null)
-                {
-                    collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayCreateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
-                }
-                else { collection = null; }
-
-                if (storeDevice == null) return BadRequest("StoreDevice not found or deleted");
-                if (menu == null && collection == null) return BadRequest("Menu/Collection not found or deleted");
-                //if (collection == null && displayCreateDTO.CollectionId != 0) return BadRequest("Collection not found or deleted");
-                if (template == null) return BadRequest("Template not found or deleted");
-
-                var data = _mapper.Map<Display>(displayCreateDTO);
-                _unitOfWork.DisplayRepository.Add(data);
-                _unitOfWork.Save();
-
-                // Adding display items
-                var productGroups = new List<ProductGroup>();
-                var boxes = new List<Box>();
-                var layers = new List<Layer>();
-                var templateWithLayer = new Template();
-                var displayItems = new List<DisplayItem>();
-
-                // GET ProductGroup List from Menu or Collection if not null
-                if (menu != null)
-                {
-                    productGroups = _unitOfWork.ProductGroupRepository.GetProductGroup(null, menu.MenuId, null);
-                }
-
-                if (collection != null)
-                {
-                    productGroups = _unitOfWork.ProductGroupRepository.GetProductGroup(null, null, collection.CollectionId);
-                }
-
-                // GET Box List from display's template
-                templateWithLayer = _unitOfWork.TemplateRepository.GetTemplateWithLayersAndBoxes(template.TemplateId);
-
-                if (templateWithLayer.Layers != null)
-                {
-                    layers.AddRange(templateWithLayer.Layers);
-
-                    foreach (var layer in layers)
-                    {
-                        if (layer.Boxes != null)
-                        {
-                            boxes.AddRange(layer.Boxes);
-                        }
-                    }
-
-                    // Query exact box in needed
-                    boxes = boxes.Where(c => c.BoxType == Domain.Models.Enum.BoxType.UseInDisplay).ToList();
-                }
-
-                // Get display items list from product groups and boxes
-                int productGroupCount = productGroups.Count;
-                int boxCount = boxes.Count;
-                //var boxesToRender = boxes.Where(c => c.)
-
-                if (boxCount < productGroupCount)
-                {
-                    _unitOfWork.DisplayRepository.Remove(data);
-                    _unitOfWork.Save();
-
-                    return BadRequest("Not enough boxes for rendering product groups.");
-                }
-
-                // Adding display items to database
-                for (int i = 0; i < productGroupCount; i++)
-                {
-                    DisplayItemCreateDTO item = new()
-                    {
-                        DisplayID = data.DisplayId,
-                        BoxID = boxes[i].BoxId,
-                        ProductGroupID = productGroups[i].ProductGroupId
-                    };
-
-                    var itemData = _mapper.Map<DisplayItem>(item);
-                    _unitOfWork.DisplayItemRepository.Add(itemData);
-                    _unitOfWork.Save();
-                }
-
+                var data = _displayService.AddDisplayV2(displayCreateDTO);
                 return CreatedAtAction(nameof(Get), new {displayId = data.DisplayId});
             }
             catch (Exception ex)
@@ -170,32 +82,31 @@ namespace SmartMenu.API.Controllers
         [HttpPut("{displayId}")]
         public IActionResult Update(int displayId, DisplayUpdateDTO displayUpdateDTO)
         {
-            var menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayUpdateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
-            if (menu == null) return BadRequest("Menu not found or deleted");
+            try
+            {
+                var data = _displayService.Update(displayId, displayUpdateDTO);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
 
-            var collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayUpdateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
-            if (collection == null) return BadRequest("Collection not found or deleted");
-
-            var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayUpdateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
-            if (template == null) return BadRequest("Template not found or deleted");
-
-            var data = _unitOfWork.DisplayRepository.Find(c => c.DisplayId == displayId && c.IsDeleted == false).FirstOrDefault();
-            if (data == null) return BadRequest("Display not found or deleted");
-
-            _mapper.Map(displayUpdateDTO, data);
-            _unitOfWork.DisplayRepository.Update(data);
-            _unitOfWork.Save();
-            return Ok(data);
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete("{displayId}")]
         public IActionResult Delete(int displayId)
         {
-            var data = _unitOfWork.DisplayRepository.Find(c => c.DisplayId == displayId && c.IsDeleted == false).FirstOrDefault();
-            if (data == null) return BadRequest("Display not found or deleted");
-            _unitOfWork.DisplayRepository.Remove(data);
-            _unitOfWork.Save();
-            return Ok();
+            try
+            {
+                _displayService.Delete(displayId);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ SmartMenu.Service/Services/DisplayService.cs
@@ -0,0 +1,225 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SmartMenu.DAO;
+using SmartMenu.Domain.Models;
+using SmartMenu.Domain.Models.DTO;
+using SmartMenu.Domain.Repository;
+using SmartMenu.Service.Interfaces;
+
+namespace SmartMenu.Service.Services
+{
+    public class DisplayService : IDisplayService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DisplayService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Display> GetAll(int? displayId, int? menuId, int? collectionId, string? searchString, int pageNumber, int pageSize)
+        {
+            var data = _unitOfWork.DisplayRepository.EnableQuery()
+                .Include(c => c.Menu)
+                .Include(c => c.Collection);
+            var result = DataQuery(data, displayId, menuId, collectionId, searchString, pageNumber, pageSize);
+
+            return result ?? Enumerable.Empty<Display>();
+        }
+
+        public IEnumerable<Display> GetWithItems(int? displayId, int? menuId, int? collectionId, string? searchString, int pageNumber, int pageSize)
+        {
+            var data = _unitOfWork.DisplayRepository.EnableQuery()
+                .Include(c => c.DisplayItems)!
+                .ThenInclude(c => c.ProductGroup)!
+                .ThenInclude(c => c!.ProductGroupItems)!
+                .ThenInclude(c => c!.Product)
+                .ThenInclude(c => c!.ProductSizePrices)
+                .Include(c => c.DisplayItems)!
+                .ThenInclude(c => c.Box)
+                .ThenInclude(c => c!.BoxItems);
+
+            var result = DataQuery(data, displayId, menuId, collectionId, searchString, pageNumber, pageSize);
+
+            return result ?? Enumerable.Empty<Display>();
+        }
+
+        public Display AddDisplay(DisplayCreateDTO displayCreateDTO)
+        {
+            if (displayCreateDTO.MenuId != null && displayCreateDTO.CollectionId != null) throw new Exception("MenuId and CollectionId cannot be using at the same time");
+
+            var storeDevice = _unitOfWork.StoreDeviceRepository.Find(c => c.StoreDeviceId == displayCreateDTO.StoreDeviceId && c.IsDeleted == false).FirstOrDefault();
+            var menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayCreateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
+            var collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayCreateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
+            var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayCreateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
+
+            if (storeDevice == null) throw new Exception("StoreDevice not found or deleted");
+            if (menu == null) throw new Exception("Menu not found or deleted");
+            if (collection == null) throw new Exception("Collection not found or deleted");
+            if (template == null) throw new Exception("Template not found or deleted");
+
+            var data = _mapper.Map<Display>(displayCreateDTO);
+            _unitOfWork.DisplayRepository.Add(data);
+            _unitOfWork.Save();
+
+            return data;
+        }
+
+        public Display AddDisplayV2(DisplayCreateDTO displayCreateDTO)
+        {
+            if (displayCreateDTO.MenuId == 0) displayCreateDTO.MenuId = null;
+            if (displayCreateDTO.CollectionId == 0) displayCreateDTO.CollectionId = null;
+
+            var storeDevice = _unitOfWork.StoreDeviceRepository.Find(c => c.StoreDeviceId == displayCreateDTO.StoreDeviceId && c.IsDeleted == false).FirstOrDefault();
+            var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayCreateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
+
+            var menu = new Menu();
+            if (displayCreateDTO.MenuId != null)
+            {
+                menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayCreateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
+            }
+            else { menu = null; }
+
+            var collection = new Collection();
+            if (displayCreateDTO.CollectionId != null)
+            {
+                collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayCreateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
+            }
+            else { collection = null; }
+
+            if (storeDevice == null) throw new Exception("StoreDevice not found or deleted");
+            if (menu == null && collection == null) throw new Exception("Menu/Collection not found or deleted");
+            //if (collection == null && displayCreateDTO.CollectionId != 0) return BadRequest("Collection not found or deleted");
+            if (template == null) throw new Exception("Template not found or deleted");
+
+            var data = _mapper.Map<Display>(displayCreateDTO);
+            _unitOfWork.DisplayRepository.Add(data);
+            _unitOfWork.Save();
+
+            // Adding display items
+            var productGroups = new List<ProductGroup>();
+            var boxes = new List<Box>();
+            var layers = new List<Layer>();
+            var templateWithLayer = new Template();
+            var displayItems = new List<DisplayItem>();
+
+            // GET ProductGroup List from Menu or Collection if not null
+            if (menu != null)
+            {
+                productGroups = _unitOfWork.ProductGroupRepository.GetProductGroup(null, menu.MenuId, null);
+            }
+
+            if (collection != null)
+            {
+                productGroups = _unitOfWork.ProductGroupRepository.GetProductGroup(null, null, collection.CollectionId);
+            }
+
+            // GET Box List from display's template
+            templateWithLayer = _unitOfWork.TemplateRepository.GetTemplateWithLayersAndBoxes(template.TemplateId);
+
+            if (templateWithLayer.Layers != null)
+            {
+                layers.AddRange(templateWithLayer.Layers);
+
+                foreach (var layer in layers)
+                {
+                    if (layer.Boxes != null)
+                    {
+                        boxes.AddRange(layer.Boxes);
+                    }
+                }
+
+                // Query exact box in needed
+                boxes = boxes.Where(c => c.BoxType == Domain.Models.Enum.BoxType.UseInDisplay).ToList();
+            }
+
+            // Get display items list from product groups and boxes
+            int productGroupCount = productGroups.Count;
+            int boxCount = boxes.Count;
+            //var boxesToRender = boxes.Where(c => c.)
+
+            if (boxCount < productGroupCount)
+            {
+                _unitOfWork.DisplayRepository.Remove(data);
+                _unitOfWork.Save();
+
+                throw new Exception("Not enough boxes for rendering product groups.");
+            }
+
+            // Adding display items to database
+            for (int i = 0; i < productGroupCount; i++)
+            {
+                DisplayItemCreateDTO item = new()
+                {
+                    DisplayID = data.DisplayId,
+                    BoxID = boxes[i].BoxId,
+                    ProductGroupID = productGroups[i].ProductGroupId
+                };
+
+                var itemData = _mapper.Map<DisplayItem>(item);
+                _unitOfWork.DisplayItemRepository.Add(itemData);
+                _unitOfWork.Save();
+            }
+
+            return data;
+        }
+
+        public Display Update(int displayId, DisplayUpdateDTO displayUpdateDTO)
+        {
+            var menu = _unitOfWork.MenuRepository.Find(c => c.MenuId == displayUpdateDTO.MenuId && c.IsDeleted == false).FirstOrDefault();
+            if (menu == null) throw new Exception("Menu not found or deleted");
+
+            var collection = _unitOfWork.CollectionRepository.Find(c => c.CollectionId == displayUpdateDTO.CollectionId && c.IsDeleted == false).FirstOrDefault();
+            if (collection == null) throw new Exception("Collection not found or deleted");
+
+            var template = _unitOfWork.TemplateRepository.Find(c => c.TemplateId == displayUpdateDTO.TemplateId && c.IsDeleted == false).FirstOrDefault();
+            if (template == null) throw new Exception("Template not found or deleted");
+
+            var data = _unitOfWork.DisplayRepository.Find(c => c.DisplayId == displayId && c.IsDeleted == false).FirstOrDefault();
+            if (data == null) throw new Exception("Display not found or deleted");
+
+            _mapper.Map(displayUpdateDTO, data);
+            _unitOfWork.DisplayRepository.Update(data);
+            _unitOfWork.Save();
+
+            return data;
+        }
+
+        public void Delete(int displayId)
+        {
+            var data = _unitOfWork.DisplayRepository.Find(c => c.DisplayId == displayId && c.IsDeleted == false).FirstOrDefault();
+            if (data == null) throw new Exception("Display not found or deleted");
+            _unitOfWork.DisplayRepository.Remove(data);
+            _unitOfWork.Save();
+        }
+
+        private static IEnumerable<Display> DataQuery(IQueryable<Display> data, int? displayId, int? menuId, int? collectionId, string? searchString, int pageNumber, int pageSize)
+        {
+            data = data.Where(c => c.IsDeleted == false);
+
+            if (displayId != null)
+            {
+                data = data.Where(c => c.DisplayId == displayId);
+            }
+
+            if (menuId != null)
+            {
+                data = data.Where(c => c.MenuId == menuId);
+            }
+
+            if (collectionId != null)
+            {
+                data = data.Where(c => c.CollectionId == collectionId);
+            }
+
+            if (searchString != null)
+            {
+                data = data.Where(c => c.ActiveHour.ToString().Contains(searchString));
+            }
+
+            return PaginatedList<Display>.Create(data, pageNumber, pageSize);
+        }
+    }
+}
