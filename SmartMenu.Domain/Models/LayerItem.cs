c51661c18cb107df9a5f5978b4ce73a5bb62d4d9
pytas0811 <tekato2002@gmail.com>
2024-06-29 13:25:32 +0700

    Update Layer

--- SmartMenu.Domain/Models/LayerItem.cs
+++ SmartMenu.Domain/Models/LayerItem.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartMenu.Domain.Models
 {
     public class LayerItem : BaseModel
     {
         public int LayerItemId { get; set; }
-
+        public int LayerId { get; set; }
         [Required]
         public string LayerItemValue { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(LayerId))]
+        public Layer? Layer {  get; set; }
     }
 }
